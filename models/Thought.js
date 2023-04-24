364966a23f0e50f32f48f94bf1883ba8b6522c62
TheRealGeneParmesan <geneparmigiano@gmail.com>
2023-04-23 22:39:33 -0700

    Created the Thought model & virtual

---
+++ models/Thought.js
@@ -0,0 +1,47 @@
+const { Schema, Types } = require('mongoose');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtID: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Reaction'
+            }
+        ],
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false,
+    });
+
+// Create a virtual property 'reactionCount' that retrieves the length of the thought's reactions array field on query.
+ThoughtSchema
+    .virtual('reactionCount')
+    // Getter
+    .get(function () {
+        return this.reactions.length;
+    });
+
+// Create the Thought model using the ThoughtSchema
+const Thought = model('Thought', ThoughtSchema);
+module.exports = Thought;
